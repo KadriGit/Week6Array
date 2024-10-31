fa669026638ee08d93c7b8c8bcd84699203a9ca3
KadriGit <kadri.viira@tktk.ee>
2024-10-31 21:55:26 +0200

    code

--- Week6Array/Program.cs
+++ Week6Array/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] fruitbasket = { "apples", "bananas", "Oranges" };
+
+Console.WriteLine($"There are {fruitbasket.Length} fruit in the basket.");
+
+//Console.WriteLine(fruitbasket[0]);
+//Console.WriteLine(fruitbasket[1]);
+//Console.WriteLine(fruitbasket[2]);
+
+fruitbasket[0] = "pine-apple";
+fruitbasket[1] = "peaches";
+
+//Console.WriteLine(fruitbasket[0]);
+//Console.WriteLine(fruitbasket[1]);
+//Console.WriteLine(fruitbasket[2]);
+
+for (int i = 0; i < fruitbasket.Length; i++)
+{
+    Console.WriteLine(fruitbasket[i]);
+}
